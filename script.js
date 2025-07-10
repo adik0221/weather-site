7f25bec236f2631c4b98750b72790c05d3734e2f
ADITYA KUMAR <akjchamp123@gmail.com>
2025-07-10 15:15:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "09083454d60a12a65c71e0f49cb65c32";
+    //const api_key = "09083454d60a12a65c71e0f49cb65c32";//
     const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
